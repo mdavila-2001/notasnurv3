bac3b7e40dadc69225cff6f4eaef03cf039a264c
Fernando.T <fernandoterrzasfiligrana@gmail.com>
2026-04-12 19:40:54 -0400

    Interfaz de usuarios completadas

---
+++ src/app/core/services/user.ts
@@ -0,0 +1,33 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class User {
+  private http = inject(HttpClient);
+  
+  private apiUrl = 'http://localhost:8080/api/users'; 
+
+  
+  getUsersByRole(role: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/role/${role}`);
+  }
+
+  create(userData: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, userData);
+  }
+
+update(id: string, userData: any): Observable<any> {
+  return this.http.put<any>(`${this.apiUrl}/${id}`, userData);
+}
+
+  updateStatus(id: string, status: string): Observable<any> {
+    return this.http.patch<any>(`${this.apiUrl}/${id}/status`, { status });
+  }
+
+  delete(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  }
+}
